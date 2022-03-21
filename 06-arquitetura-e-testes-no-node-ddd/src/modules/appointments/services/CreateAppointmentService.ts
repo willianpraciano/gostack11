d50b41a18c822991040894946eb35fec9d0ae3f3
Willian S. Praciano <willian.s.praciano@gmail.com>
2022-03-21 08:56:38 -0300

    12.1 - 8: Dependency Inversion Principle

--- 06-arquitetura-e-testes-no-node-ddd/src/modules/appointments/services/CreateAppointmentService.ts
+++ 06-arquitetura-e-testes-no-node-ddd/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,33 +1,31 @@
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
-interface Request {
+interface IRequest {
   provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  //retorno de funçao async é sempre uma Promise
-  public async execute({ date, provider_id }: Request): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  constructor(private appointmentsRepository: IAppointmentsRepository) {}
 
+  //retorno de funçao async é sempre uma Promise
+  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
-      appointmentDate,
-    );
+    const findAppointmentInSameDate =
+      await this.appointmentsRepository.findByDate(appointmentDate);
 
     if (findAppointmentInSameDate) {
       throw new AppError('This appointment is already booked');
     }
 
     //Cria a instância do Appointment mas não salva no banco de dados
-    const appointment = await appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
