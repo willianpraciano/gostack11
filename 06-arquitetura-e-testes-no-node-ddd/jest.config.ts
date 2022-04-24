e09f2f9d6d1d62380b41fb05e2f14a3c686ee2c0
Willian S. Praciano <willian.s.praciano@gmail.com>
2022-04-23 21:25:45 -0300

    12.2 - 4 Creating the first test

--- 06-arquitetura-e-testes-no-node-ddd/jest.config.ts
+++ 06-arquitetura-e-testes-no-node-ddd/jest.config.ts
@@ -3,6 +3,9 @@
  * https://jestjs.io/docs/configuration
  */
 
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
 export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -81,7 +84,9 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src/',
+  }),
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -93,7 +98,7 @@ export default {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  preset: 'ts-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
--- 06-arquitetura-e-testes-no-node-ddd/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ 06-arquitetura-e-testes-no-node-ddd/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,21 @@
-test('Sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointments', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointments = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointment = await createAppointments.execute({
+      date: new Date(),
+      provider_id: '123234546',
+    });
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123234546');
+  });
+
+  // it('not should be able to create two appointment on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
 });
