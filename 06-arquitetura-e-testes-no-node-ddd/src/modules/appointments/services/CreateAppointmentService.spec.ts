85b6a54b180bf66f4ee7aa8f549e7efe60d102c7
Willian S. Praciano <willian.s.praciano@gmail.com>
2022-04-26 09:18:19 -0300

    12.2 - 6 Finishing appointment tests

--- 06-arquitetura-e-testes-no-node-ddd/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ 06-arquitetura-e-testes-no-node-ddd/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -15,7 +16,24 @@ describe('CreateAppointments', () => {
     expect(appointment.provider_id).toBe('123234546');
   });
 
-  // it('not should be able to create two appointment on the same time', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('not should be able to create two appointment on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointments = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2022, 4, 10, 11);
+
+    await createAppointments.execute({
+      date: appointmentDate,
+      provider_id: '123234546',
+    });
+
+    expect(
+      createAppointments.execute({
+        date: appointmentDate,
+        provider_id: '123234546',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
