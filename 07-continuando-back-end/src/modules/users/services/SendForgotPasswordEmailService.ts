3c7d9725f0b28dcd8e25f6b7e6cbdf2fd9c19f3e
Willian S. Praciano <willian.s.praciano@gmail.com>
2022-05-13 09:20:47 -0300

    13.2 Recovering password

--- 07-continuando-back-end/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ 07-continuando-back-end/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import IUserRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 
 import User from '../infra/typeorm/entities/User';
@@ -18,8 +19,20 @@ class SendForgotPasswordEmailService {
 
     @inject('MailProvider')
     private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
+
   public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
     this.mailProvider.sendMail(
       email,
       'Pedido de recuperação de senha recebido.',
