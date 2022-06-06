d9c19f5da3d32bc95dc81cf5dcb1c42dfbee1554
Willian S. Praciano <willian.s.praciano@gmail.com>
2022-06-06 17:52:02 -0300

    13.3 Excluding old times

--- 07-continuando-back-end/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ 07-continuando-back-end/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -43,16 +43,18 @@ export default class ListProviderDayAvailabilityService {
       (_, index) => index + hourStart,
     );
 
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHour = appointments.find(
         appointment => getHours(appointment.date) === hour,
       );
 
-      console.log(hasAppointmentInHour);
+      const compareDate = new Date(year, month - 1, day, hour);
 
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
       };
     });
 
