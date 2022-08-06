38b586e5f46fa919e22f84e0c8ab9f6174b1e069
Willian S. Praciano <willian.s.praciano@gmail.com>
2022-08-06 11:00:02 -0300

    14.3 refactoring tests

--- 08-finalizando-back-end/src/config/auth.ts
+++ 08-finalizando-back-end/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: process.env.APP_SECRET ?? '1ea4a45d8a9e2e4f4c1e498b8b5335f8',
+    secret: process.env.APP_SECRET || 'default',
     expiresIn: '1d',
   },
 };
--- 08-finalizando-back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ 08-finalizando-back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,20 +1,24 @@
 import AppError from '@shared/errors/AppError';
 import FakeNotificationsRepository from '@modules/notifications/repositories/fakes/FakeNotificationsRepository';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
 import CreateAppointmentService from './CreateAppointmentService';
 
 let fakeAppointmentsRepository: FakeAppointmentsRepository;
 let fakeNotificationsRepository: FakeNotificationsRepository;
+let fakeCacheProvider: FakeCacheProvider;
 let createAppointments: CreateAppointmentService;
 
 describe('CreateAppointments', () => {
   beforeEach(() => {
     fakeAppointmentsRepository = new FakeAppointmentsRepository();
     fakeNotificationsRepository = new FakeNotificationsRepository();
+    fakeCacheProvider = new FakeCacheProvider();
 
     createAppointments = new CreateAppointmentService(
       fakeAppointmentsRepository,
       fakeNotificationsRepository,
+      fakeCacheProvider,
     );
   });
 
