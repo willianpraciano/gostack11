4b07568e3ee252ebf88fcaedce0f8dd3c4b309fc
Willian S. Praciano <willian.s.praciano@gmail.com>
2022-08-11 19:07:31 -0300

    9.2 styling dashbord

--- 03-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ 03-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,11 +1,54 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 export function Dashboard() {
   return (
-    <Container>
-      <h1>Explore repositórios no Github</h1>;
-    </Container>
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>;
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="">
+          <img
+            src="https://avatars.githubusercontent.com/u/28768280?v=4"
+            alt="Willian S. Praciano"
+          />
+          <div>
+            <strong>unform</strong>
+            <p>Esta é uma super descrição</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="">
+          <img
+            src="https://avatars.githubusercontent.com/u/28768280?v=4"
+            alt="Willian S. Praciano"
+          />
+          <div>
+            <strong>unform</strong>
+            <p>Esta é uma super descrição</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="">
+          <img
+            src="https://avatars.githubusercontent.com/u/28768280?v=4"
+            alt="Willian S. Praciano"
+          />
+          <div>
+            <strong>unform</strong>
+            <p>Esta é uma super descrição</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
   );
 }
