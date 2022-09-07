33ee366aee9897de5b6a65f9602dd4988ed4cbf6
Willian S. Praciano <willian.s.praciano@gmail.com>
2022-09-07 16:16:56 -0300

    9.2 saving in local storage

--- 03-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ 03-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -19,7 +19,22 @@ interface Repository {
 export function Dashboard() {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storegeRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+    if (storegeRepositories) {
+      return JSON.parse(storegeRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
   async function handleAddRepository(event: FormEvent) {
     event.preventDefault();
