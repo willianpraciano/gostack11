32d16c18325f7e7c31c5f15612f6de1a4db8c08c
Willian S. Praciano <willian.s.praciano@gmail.com>
2022-09-09 23:23:58 -0300

    9.2 style repository details screen

--- 03-primeiro-projeto-react/src/pages/Repository/index.tsx
+++ 03-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,11 +1,58 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-// import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 export function Repository() {
   const { repository } = useParams();
-  return <h1>aaa {repository}</h1>;
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/dashboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars.githubusercontent.com/u/79586352?s=200&v=4" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to={`sdfds`}>
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 }
 
 export default Repository;
