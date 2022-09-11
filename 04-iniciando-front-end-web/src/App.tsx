9ad749de913a1629a4062d3d420a38c503011f16
Willian S. Praciano <willian.s.praciano@gmail.com>
2022-09-11 18:00:55 -0300

    10.1 - creating global styles

--- 04-iniciando-front-end-web/src/App.tsx
+++ 04-iniciando-front-end-web/src/App.tsx
@@ -1,7 +1,14 @@
 import { useState } from 'react';
 
+import GobalStyle from './styles/global';
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <h1>Hello world</h1>
+      <GobalStyle />
+    </>
+  );
 }
 
 export default App;
