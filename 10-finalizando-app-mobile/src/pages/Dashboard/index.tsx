826048b5e2c973ee9fc6ecaef494306021ab5115
Willian S. Praciano <willian.s.praciano@gmail.com>
2022-11-15 21:16:48 -0300

    16.2 - create dashboard header

--- 10-finalizando-app-mobile/src/pages/Dashboard/index.tsx
+++ 10-finalizando-app-mobile/src/pages/Dashboard/index.tsx
@@ -1,15 +1,36 @@
-import { View, Button } from 'react-native';
-
+import { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
 
 export function Dashboard() {
-  const { signOut } = useAuth();
+  const { user } = useAuth();
+  const { navigate } = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
     <Container>
-      <Button title="Sair" onPress={signOut} color="#ff9000" />
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
     </Container>
   );
 }
