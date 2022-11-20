540b5ca20fbedd79e3f3b369b38d58e25e0faf88
Willian S. Praciano <willian.s.praciano@gmail.com>
2022-11-20 10:49:53 -0300

    16.4 - create success screen structure

--- 10-finalizando-app-mobile/src/pages/AppointmentCreated/index.tsx
+++ 10-finalizando-app-mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,5 +1,36 @@
-import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
+import { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
 export function AppointmentCreated() {
-  return <Container />;
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>
+        Terça, dia 14 de março de 2020 às 12:00h com Diego
+      </Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 }
